eae027d9bd788645641c1b102444a1296971613e
Michael Kobiliansky <mishakob@gmail.com>
2014-10-25 21:27:52 -0700

    updated location
    updated Github location in UI file

--- UI.R
+++ UI.R
@@ -11,7 +11,7 @@ shinyUI(pageWithSidebar(
         ),
   mainPanel(
     p("This is a simple prediction of a death year, based on input of birth year and gender.",
-      "The source code can be found ",a("here",href="http://github.com/michaelstanhope/shiny-examples"),"."),
+      "The source code can be found ",a("here",href="http://github.com/mishakob/project3"),"."),
     h4("How to use this application"),
     p("Enter the birth year and the gender on the left of the page. The predicted death year would show below."),
     h4('Your predicted death year is...'),
